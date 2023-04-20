2275f02b2606bb86284063f0e76277ddea966b73
TomasCarvalho <2606tom@gmail.com>
2023-04-20 18:21:22 +0100

    Add player enemy selection

--- GameSix/Program.cs
+++ GameSix/Program.cs
@@ -6,12 +6,22 @@ namespace GameSix
     {
         static void Main(string[] args)
         {
-            Foe goblin = new Foe("  Goblin ");
+            Console.WriteLine("Input nº of enemies:");
+            int enemyNum=int.Parse(Console.ReadLine());
 
-            Console.WriteLine(goblin.GetName());
-            Console.WriteLine(goblin.GetHealth());
+            Foe [] foes= new Foe[enemyNum];
 
-
+            for (int i = 0; i < foes.Length; i++)
+            {
+                Console.WriteLine($"Input enemy #{i+1}'s name:");
+                foes[i] = new Foe(Console.ReadLine());
+            }
+            
+            Console.WriteLine("Enemies:");
+            for (int i = 0; i < foes.Length; i++)
+            {
+                Console.WriteLine($"{i+1}-{foes[i].GetName()}");
+            }
         }
     }
 }
