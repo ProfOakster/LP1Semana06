96fd2997779fb00e81eb9d7c60d61afde06079de
TomasCarvalho <2606tom@gmail.com>
2023-04-21 10:23:17 +0100

    Add class Foe and enum PowerUp from GameSix;Replace class Program

---
+++ GameSixFriday/Foe.cs
@@ -0,0 +1,69 @@
+namespace GameSixFriday
+{
+    public class Foe
+    {
+        private string name;
+        private float health;
+        private float shield;
+        private static int powerupCount;
+
+        public Foe(string name)
+        {
+            this.SetName(name);
+            health = 100;
+            shield = 0;
+        }
+        static Foe()
+        {
+            powerupCount = 0;
+        }
+
+        public string GetName() => name;
+
+        public float GetHealth() => health;
+
+        public float GetShield() => shield;
+
+        public static int GetPowerupCount() => powerupCount;
+
+
+        public void SetName(string name)
+        {
+            this.name = name.Trim();
+        }
+
+        public void TakeDamage(float damage)
+        {
+            shield -= damage;
+            if (shield < 0)
+            {
+                float damageStillToInflict = -shield;
+                shield = 0;
+                health -= damageStillToInflict;
+                if (health < 0) health = 0;
+            }
+        }
+
+        public void PickupPowerUp(PowerUp powerup, float bonus)
+        {
+            if (powerup == PowerUp.Health)
+            {
+                health += bonus;
+                if (health > 100)
+                    health = 100;
+            }
+            if (powerup == PowerUp.Shield)
+            {
+                shield += bonus;
+                if (shield > 100)
+                    shield = 100;
+            }
+
+            powerupCount += 1;
+        }
+
+
+
+
+    }
+}
