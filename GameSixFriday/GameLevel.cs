1264a873cd4eb059baa6cd4f3fc46c63a04b02c4
TomasCarvalho <2606tom@gmail.com>
2023-04-21 10:50:15 +0100

    Add method SetFoeInRoom to class GameLevel

--- GameSixFriday/GameLevel.cs
+++ GameSixFriday/GameLevel.cs
@@ -2,6 +2,39 @@ namespace GameSixFriday
 {
     public class GameLevel
     {
+        private int numRooms;
+        private int numFoes;
+        private Difficulty difficulty;
+        private Foe[] rooms;
+
+
+        public GameLevel(int numRooms, Difficulty difficulty)
+        {
+            this.numRooms = numRooms;
+            numFoes = 0;
+            this.difficulty = difficulty;
+            rooms = new Foe[numRooms];
+
+        }
+
+        public int GetNumRooms() => numRooms;
+        public int GetNumFoes() => numFoes;
+        public Difficulty GetDifficulty() => difficulty;
+
+
+        public void SetFoeInRoom(int roomIndex, Foe foe)
+        {
+            rooms[roomIndex] = foe;
+            numFoes += 1;
+        }
+
+
+        public void PrintFoes()
+        {
+
+        }
+
+
 
     }
 }
